Updated scrape to include first round. File structure changed to allow for multiple types of normalization. Bugs fixed in weightedKNN so that it now actually works. kNN also works.

Playoffs/knn.py
@@ -11,7 +11,6 @@ class Team:
 
 #ignores last entry b/c that's assumed to be the label
 def dist(a,b):
-    print "DIST", a,b
     arr_a= np.array(a.attr)
     arr_b= np.array(b.attr)
     return np.linalg.norm(arr_a-arr_b)
@@ -21,12 +20,11 @@ def csvToLists(csv, data_type):
     datafile = open(csv, 'r')
     data = []
     for row in datafile:
-        stats= [float(elem) for elem in row.strip().split(',')]
+        stats= [elem for elem in row.strip().split(',')]
         if data_type=="train":
-            print stats, "ooga", stats[-2], stats[-1]
-            data.append(Team(stats[-2], stats[:-2], stats[-1], sys.maxint))
+            data.append(Team(stats[-2], [float(elem) for elem in stats[:-2]], float(stats[-1]), sys.maxint))
         elif data_type=="test":
-            data.append(Team(stats[-1], stats[:-1], "", sys.maxint))
+            data.append(Team(stats[-1], [float(elem) for elem in stats[:-1]], "", sys.maxint))
         else:
             raise Exception("data_type must be \"train\" or \"test\"!")
     return data
@@ -44,6 +42,7 @@ def getNearestNeighbors(k, trainSet, testPoint):
         #if there aren't k neighbors yet, add one
         if(len(kClosest) < k):
             kClosest += [example]
+            #print example.url, example.label, example.sim
             if(distBound < example.sim):
                 distBound= example.sim
                 furthestIndex= len(kClosest)-1
@@ -87,20 +86,21 @@ def weightedKNN(k,trainSet,testPoint):
     kClosest= getNearestNeighbors(k, trainSet, testPoint)
     #majority vote, can be made more efficient
     kClosest= teamSort(kClosest)
-    mode= kClosest[0].label #guaranteed to exist (so no array bounds issue)
-    modeFreq= 0
-    currMode= kClosest[0].label
-    currModeFreq= 0
+    weighted_total= 0 #guaranteed to exist (so no array bounds issue)
+    sum_of_weights= 0
     for neighbor in kClosest:
-        if(neighbor.label == currMode):
-            currModeFreq += (1/neighbor.label if neighbor.label!=0 else sys.maxint) #when neighbor[1], currModeFreq= infinity
-            if(currModeFreq > modeFreq):
-                modeFreq= currModeFreq
-                mode= currMode
-        else:
-            currMode= neighbor.label
-            currModeFreq= (1/neighbor.label if neighbor.label!=0 else sys.maxint)
-    return mode
+        weight= (1/neighbor.sim if neighbor.sim!=0 else sys.maxint/20)
+        print neighbor.url, neighbor.label, neighbor.sim
+        sum_of_weights += weight
+        weighted_total += weight * neighbor.label
+    return weighted_total/sum_of_weights
 
 if __name__=="__main__":
-    print weightedKNN(5,csvToLists("/Users/nikhilnathwani/Desktop/BBall/Playoffs/training_normalized/all_stats_normalized.csv", "train"),csvToLists("/Users/nikhilnathwani/Desktop/BBall/Playoffs/test_normalized/all_stats2014_normalized.csv", "test")[0])
+    k= 10
+    train= csvToLists("/Users/nikhilnathwani/Desktop/BBall/Playoffs/training/raw/all_stats", "train")
+    test= csvToLists("/Users/nikhilnathwani/Desktop/BBall/Playoffs/test/raw/all_stats2014", "test")
+    for team in test:
+        print "\n-------------------------"
+        print k, "Closest neighbors of:", team.url
+        print weightedKNN(k, train, team)
+        print "-------------------------\n"